e0f4da71b9bd720f85807f1b6d0824fb846fa800
Jordan Williams <jjwilli13@gmail.com>
2022-08-12 05:14:28 -0500

    created repo

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
