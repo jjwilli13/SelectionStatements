b4c9bc8efb26a3d4189e7f2a8ba0de90bb82b6c4
Jordan Williams <jjwilli13@gmail.com>
2022-08-17 05:53:30 -0500

    initial commit

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,40 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess my favorite number.");
+            Console.WriteLine("Hint, the number is between 1 and 25");
+            
+
+            var r = new Random();
+            var favNumber = r.Next(1, 25);
+            int userInput;
+
+            do
+            {
+                Console.WriteLine("Take your guess:");
+
+               userInput = int.Parse(Console.ReadLine());
+
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too High");
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else
+                {
+                    Console.WriteLine("Nevermind");
+                }
+            } while (userInput != favNumber );
+            
+
+
+
+
         }
     }
 }
